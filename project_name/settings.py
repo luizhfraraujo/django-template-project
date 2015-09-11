e865d417b91b27c0d3b73f26f4a7dccfae16889c
luizhfra <luizhfreitas@outlook.com>
2015-09-11 15:22:36 -0300

    UPDATE Vars
    {{project_name}}

--- project_name/settings.py
+++ project_name/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project_name.accounts'
+    '{{project_name}}.accounts'
 )
 
 MIDDLEWARE_CLASSES = (
