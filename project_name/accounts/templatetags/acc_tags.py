f8630dd829a84e07a902e5bb70295097e0a97d45
luizhfra <luizhfreitas@outlook.com>
2015-11-10 17:06:53 -0200

    add templatetag

--- project_name/accounts/templatetags/acc_tags.py
+++ project_name/accounts/templatetags/acc_tags.py
@@ -1,7 +1,13 @@
 from django.template import Library
+from django import template
 
 register = Library()
 
 @register.filter(name='addcssclass')
 def addcssclass(field, css):
    return field.as_widget(attrs={"class":css})
+
+@register.filter
+def get_type(value):
+	t = type(value)
+	return t.__module__ + "." + t.__name__
